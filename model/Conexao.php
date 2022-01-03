d82273cae0f8f95b088dc28bc77f373e3f43747b
Maria Rayane de Medeiros Silva <rayane.medeiros@academico.ifrn.edu.br>
2022-01-03 17:47:52 -0300

    Adicionando a classe de conexão

---
+++ model/Conexao.php
@@ -0,0 +1,71 @@
+<?php
+
+class Conexao{
+
+    public static function
+    selectById($tabela, $colunas, $id){
+        $sql = "SELECT $colunas FROM $tabela 
+        WHERE id=$id;";
+        $recurso = self::getConexao()
+            ->prepare($sql);
+        $recurso->execute();
+
+        return $recurso->fetchAll();
+    }
+
+    public static function
+    select($tabela, $colunas){
+        $sql = "SELECT $colunas FROM $tabela;";
+        $recurso = self::getConexao()
+            ->prepare($sql);
+        $recurso->execute();
+
+        return $recurso->fetchAll();
+    }
+
+    public static function
+    update($tabela, $parametros, $id){
+        $sql = "UPDATE $tabela SET $parametros 
+        WHERE id=$id;";
+        $recurso = self::getConexao()
+            ->prepare($sql);
+        $recurso->execute();
+
+    }
+
+    public static function 
+    delete($tabela, $id){
+        $sql = "DELETE FROM $tabela WHERE 
+        id=$id;";
+        Conexao::getConexao()->exec($sql);
+        echo $sql;
+    }
+
+    public static function 
+    insert($tabela, $colunas, $valores){
+        $sql = "INSERT INTO ". $tabela . "
+        (" . $colunas . ") VALUES 
+        (". $valores .");";
+        Conexao::getConexao()->exec($sql);
+        echo $sql;
+    }
+
+    private static function getConexao(){
+        try{
+            $conexao = new PDO(
+                "mysql:host=localhost;dbname=ppi4v_guloseimas",
+                "ppi4v",
+                "ppi42@ifrn"
+            );
+            $conexao->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+            //echo "Voilá !";
+            return $conexao;
+        }
+        catch(PDOException $e){
+            echo "Deu tiut: " . $e->getMessage();
+        }
+    }
+}
